4fa347f91d306c341caf44a501f244cfe343e11e
Billy Poon <panqb@uctrl.cn>
2023-12-06 15:05:30 +0800

    fix extension problem

--- src/configuration.ts
+++ src/configuration.ts
@@ -4,7 +4,7 @@ import { join } from 'path'
 @Configuration({
   namespace: 'cli',
   importConfigs: [
-    join(__dirname, 'config/config.default.ts')
+    join(__dirname, 'config/config.default')
   ]
 })
 export class CLIConfiguration {
